90b3bbf789ab6f9d2cb024e6e83a04d5a6e63fdb
jazmino27 <35322527+jazmino27@users.noreply.github.com>
2018-01-28 23:26:31 -0600

    Add files via upload
    Asking for unique character of my dog's name...not getting return, false but I'm not yet sure how/where to fit in - will continue to try

---
+++ Assignment 2 Unique Array Committ 1.js
@@ -0,0 +1,15 @@
+function unique (array)
+{
+    var str=array;
+    var unique="";
+    for (var x=0;x < str.length;x++)
+    {
+
+        if (!unique.array) {
+            continue;
+        }
+        unique += str[x];
+    }
+    return unique;
+}
+console.log(unique ("Gizmo Ortiz"));
