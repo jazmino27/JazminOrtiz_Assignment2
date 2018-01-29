41681917785f05d0ee7f788b89777633a3a4434c
jazmino27 <35322527+jazmino27@users.noreply.github.com>
2018-01-28 23:57:59 -0600

    Add files via upload
    struggled with DeepEqual - will review at office hours

---
+++ Assignment2DeepEqualCommitt1.js
@@ -0,0 +1,24 @@
+// Deep Equal Exercise
+
+deepEqual(4);
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(4, 4));
+// ? true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// ? false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// ? true
+
+x == "object" && x != null
+//x = 5; y = 5 my logic breakdown
+
+//true
+
+//x = 5; x = 5
+
+//true
+
+//x = 5; x = notnull
+
+//true
